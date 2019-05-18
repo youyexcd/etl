83eca8be65be0e5d65b6b11249b039bd090cd1fd
xcd <youyexcd@163.com>
2019-05-19 01:47:44 +0800

    add smtp function in utils add remove test code from py file to local dir test_case

--- utils.py
+++ utils.py
@@ -3,9 +3,13 @@
 
 
 import configparser
-import pymysql
-import sqlalchemy
+import smtplib
+from email.mime.text import MIMEText
 
+#import pymysql
+#import sqlalchemy
+#from sqlalchemy.orm import sessionmaker
+#import job_store
 
 DB_CONFIG_PATH = 'config/db.config'
 
@@ -21,15 +25,38 @@ def read_sql_script(script_path):
         sql_list = script.read().split(';');
     return sql_list
 
+def get_db_connection(db_name):
+    db_config = get_db_config_by_section(db_name)
+    con_string =("mysql+pymysql://%s:%s@%s:%s/%s" % (db_config['username'], db_config['password'], db_config['host'], db_config['port'], db_config['database']))
+    return con_string
 
-con = pymysql.connect('127.0.0.1', 'root', 'password')
-cursor = con.cursor()
 
-a = read_sql_script('test_data/init_tables.sql')
-for sql in a:
-    print(sql)
-    cursor.execute(sql)
-    print('-----')
-#engine = sqlalchemy.create_engine("mysql+mysqldb://root:password@127.0.0.1:3306/DB1")
+def set_mail_msg(from_addr,to_addr,sub,body):
+    msg = MIMEText(body,'plain','utf-8')
+    msg['Subject'] = sub
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    return msg
+
+
+def get_smtp_server(smtp_host, smtp_port, username, password):
+    smtp_server = smtplib.SMTP(smtp_host, smtp_port)
+    smtp_server.login(username, password)
+    return smtp_server
+
+
+def send_mail(smtp_server,from_addr,to_addr,msg):
+    try:
+        smtp_server.sendmail(from_addr, to_addr, msg.as_string())
+    except Exception:
+        return False
+    return True
+
+
+
+
+
+
+
 
 
